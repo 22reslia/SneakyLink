e1c53abffcaef21b445e4511929df3b513f7ef33
22reslia <132597444+22reslia@users.noreply.github.com>
2024-09-20 02:20:16 -0400

    Removed default exit commands

--- Game1.cs
+++ Game1.cs
@@ -32,8 +32,6 @@ public class Game1 : Game
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
 
         // TODO: Add your update logic here
 
