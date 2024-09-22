663d7efab3f7ba4c3462f8e19daf47cb2b36ba5e
Nacho-S07 <a1317868187@gmail.com>
2024-09-22 04:32:30 -0400

    Upload command switch to previous block

---
+++ PreviousBlockCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneakyLink
+{
+    public class PreviousBlockCommand : ICommand
+    {
+        private Block block;
+
+        public PreviousBlockCommand(Block block)
+        {
+            this.block = block;
+        }
+
+        public void Execute()
+        {
+            block.PreviousImage();
+        }
+    }
+}
