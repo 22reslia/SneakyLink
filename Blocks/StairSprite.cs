ca55ec42ef1cf78f99deece94a411f98e55d99f3
Nacho-S07 <a1317868187@gmail.com>
2024-09-30 18:21:23 -0400

    Update new logic blocks

---
+++ Blocks/StairSprite.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneakyLink.Blocks
+{
+    public class StairSprite : ISprite
+    {
+        Texture2D image;
+        Rectangle sourceRectangle;
+
+        public StairSprite()
+        {
+            image = BlockSpriteFactory.Instance.GetSheet();
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch, int x, int y)
+        {
+            sourceRectangle = new Rectangle(1035, 28, 16, 16);
+            spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            spriteBatch.Draw(image, new Rectangle(300, 200, 32, 32), sourceRectangle, Color.White);
+            spriteBatch.End();
+        }
+    }
+}
---
+++ Blocks/WallSprite.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneakyLink.Blocks
+{
+    public class WallSprite : ISprite
+    {
+        Texture2D image;
+        Rectangle sourceRectangle;
+
+        public WallSprite()
+        {
+            image = BlockSpriteFactory.Instance.GetSheet();
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch, int x, int y)
+        {
+            sourceRectangle = new Rectangle(815, 11, 32, 32);
+            spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            spriteBatch.Draw(image, new Rectangle(300, 200, 32, 32), sourceRectangle, Color.White);
+            spriteBatch.End();
+        }
+    }
+}
