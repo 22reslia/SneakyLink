02c85c14de90612773c5ad89cd9c1c36f876f3e6
Nacho-S07 <a1317868187@gmail.com>
2024-09-30 18:20:55 -0400

    delete later

--- setRectanglesBlock.cs
+++ setRectanglesBlock.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+﻿/*using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
@@ -23,23 +23,24 @@ namespace SneakyLink
         }
         public Rectangle[] LoadBlock()
         {
-            blockSourceRectangles[0] = new Rectangle(984, 11, 16, 16);
-            blockSourceRectangles[1] = new Rectangle(1001, 11, 16, 16);
-            blockSourceRectangles[2] = new Rectangle(1018, 11, 16, 16);
-            blockSourceRectangles[3] = new Rectangle(1035, 11, 16, 16);
-            blockSourceRectangles[4] = new Rectangle(984, 28, 16, 16);
-            blockSourceRectangles[5] = new Rectangle(1001, 28, 16, 16);
-            blockSourceRectangles[6] = new Rectangle(1018, 28, 16, 16);
-            blockSourceRectangles[7] = new Rectangle(1035, 28, 16, 16);
-            blockSourceRectangles[8] = new Rectangle(984, 45, 16, 16);
-            blockSourceRectangles[9] = new Rectangle(1001, 45, 16, 16);
-            blockSourceRectangles[10] = new Rectangle(815, 11, 32, 32);
-            blockSourceRectangles[11] = new Rectangle(848, 11, 32, 32);
-            blockSourceRectangles[12] = new Rectangle(881, 11, 32, 32);
-            blockSourceRectangles[13] = new Rectangle(914, 11, 32, 32);
-            blockSourceRectangles[14] = new Rectangle(947, 11, 32, 32);
+            blockSourceRectangles[0] = new Rectangle();
+            blockSourceRectangles[1] = new Rectangle();
+            blockSourceRectangles[2] = new Rectangle();
+            blockSourceRectangles[3] = new Rectangle();
+            blockSourceRectangles[4] = new Rectangle();
+            blockSourceRectangles[5] = new Rectangle();
+            blockSourceRectangles[6] = new Rectangle();
+            blockSourceRectangles[7] = new Rectangle();
+            blockSourceRectangles[8] = new Rectangle();
+            blockSourceRectangles[9] = new Rectangle();
+            blockSourceRectangles[10] = new Rectangle();
+            blockSourceRectangles[11] = new Rectangle();
+            blockSourceRectangles[12] = new Rectangle();
+            blockSourceRectangles[13] = new Rectangle();
+            blockSourceRectangles[14] = new Rectangle();
 
             return blockSourceRectangles;
         }
     }
 }
+*/
