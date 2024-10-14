1a235ad84ea25d232d4e32114d995ee2c2e52c07
Jiashuo Cheng <chengjiashuo39@outlook.com>
2024-10-14 19:31:22 -0400

    Update CollisionObjectType

--- Collision/ICollision.cs
+++ Collision/ICollision.cs
@@ -3,7 +3,7 @@
 namespace SneakyLink.Collision
 {
     public enum CollisionType { None, Left, Right, Top, Bottom }
-    public enum CollisionObjectType { Player, Enemy, Block };
+    public enum CollisionObjectType { Player, Enemy, Block, Interactive };
 
     public interface ICollision
     {
