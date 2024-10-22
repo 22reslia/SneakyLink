f414228cdc6c108f2afcd3f8e2c94ff49e5e38d2
Jiashuo Cheng <chengjiashuo39@outlook.com>
2024-10-21 21:01:42 -0400

    Move CollisionsCheck to Collision folder

--- Enemies/IEnemy.cs
+++ Enemies/IEnemy.cs
@@ -1,11 +1,11 @@
 using Microsoft.Xna.Framework.Graphics;
 using SneakyLink.Collision;
 
-namespace SneakyLink
+namespace SneakyLink.Enemies
 {
     public interface IEnemy
     {
-        public int X {  get; set; }
+        public int X { get; set; }
         public int Y { get; set; }
         public bool isBlockedL { get; set; }
         public bool isBlockedR { get; set; }
