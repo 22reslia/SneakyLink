30551df23c9ac4da440050f456aaa6f7cdc37b4e
Nacho-SZ <sanszeng.razx@outlook.com>
2024-11-10 17:42:24 -0500

    add object file of map, bomb and rupee

---
+++ Items/BombObject.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using SneakyLink.Collision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneakyLink.Items
+{
+    public class BombObject : IItem
+    {
+        private ISprite BombSprite;
+        private int x, y;
+        private CollisionBox collisionBox;
+        public CollisionBox CollisionBox { get => collisionBox; set => collisionBox = value; }
+
+        public BombObject(int positionX, int positionY)
+        {
+            BombSprite = new MapSprite();
+            x = positionX;
+            y = positionY;
+            collisionBox = new CollisionBox(CollisionObjectType.Interactive, 24, 48, x, y);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            BombSprite.Draw(spriteBatch, x, y);
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
