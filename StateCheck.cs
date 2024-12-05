fe1354d745d9be2824422addb1374497e62c661c
22reslia <132597444+22reslia@users.noreply.github.com>
2024-12-04 22:11:24 -0500

    Link level system (Link kills things and gets faster)

--- StateCheck.cs
+++ StateCheck.cs
@@ -3,10 +3,6 @@ using SneakyLink.Enemies;
 using SneakyLink.Items;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SneakyLink
 {
@@ -14,16 +10,20 @@ namespace SneakyLink
     {
         public static void stateCheck(Game1 game)
         {
-            //check if enemy is dead, if so remove it from enemy list
             List<IEnemy> enemyDie = new List<IEnemy>();
             Random random = new Random();
-            foreach(IEnemy enemy in game.enemies)
+
+            foreach (IEnemy enemy in game.enemies)
             {
                 if (enemy.cHealth <= 0)
                 {
                     enemyDie.Add(enemy);
+
+                    // Grant XP to Link
+                    game.link.GainExperience(20); // Adjust XP gain per enemy as needed
+
                     game.itemList.Add(new RupeeObject(enemy.X, enemy.Y));
-                    int drop = random.Next(0,100);
+                    int drop = random.Next(0, 100);
                     if (drop <= 25)
                     {
                         game.itemList.Add(new HeartContainer(enemy.X + 20, enemy.Y + 20));
@@ -34,13 +34,13 @@ namespace SneakyLink
                     }
                 }
             }
-            foreach(IEnemy enemy in enemyDie)
+
+            foreach (IEnemy enemy in enemyDie)
             {
                 game.enemies.Remove(enemy);
                 game.itemList.Add(new EnemyDeath(enemy.X, enemy.Y));
             }
 
-            //check if player is dead, if so game over
             if (game.link.currentHealth <= 0)
             {
                 game.gameState = GameState.GameOver;
